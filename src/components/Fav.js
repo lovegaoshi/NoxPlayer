abb1a643bd722b962c0a0b1335f9fd922a9b2b06
unknown <106490582+lovegaoshi@users.noreply.github.com>
2022-10-22 19:49:33 -0700

    Update Fav.js

--- src/components/Fav.js
+++ src/components/Fav.js
@@ -150,13 +150,13 @@ export const Fav = (function ({ FavList, onSongIndexChange, onAddOneFromFav, han
     const [currentFavList, setCurrentFavList] = useState(null)
     const [rows, setRows] = useState(null)
     const [page, setPage] = useState(0);
-    const [rowsPerPage, setRowsPerPage] = useState(25);
+    const [rowsPerPage, setRowsPerPage] = useState(24);
 
     useEffect(() => {
         setCurrentFavList(FavList)
         setRows(FavList.songList)
         setPage(0)
-        setRowsPerPage(25)
+        setRowsPerPage(24)
         //console.log(FavList)
     }, [FavList])
 
@@ -217,7 +217,7 @@ export const Fav = (function ({ FavList, onSongIndexChange, onAddOneFromFav, han
                             </Grid>
                         </Grid>
                     </Box>
-                    <TableContainer className={className} id='FavTable' component={Paper} sx={{ maxHeight: "calc(100% - 44px)" }} style={{ overflow: "auto" }} >
+                    <TableContainer className={className} id='FavTable' component={Paper} sx={{ maxHeight: "calc(100% - 65px)" }} style={{ overflow: "auto" }} >
                         <Table stickyHeader aria-label="sticky table" >
                             <TableHead>
                                 <TableRow>
@@ -278,7 +278,7 @@ export const Fav = (function ({ FavList, onSongIndexChange, onAddOneFromFav, han
                                 <TableRow>
                                     <ThemeProvider theme={theme}>
                                         <TablePagination
-                                            rowsPerPageOptions={[25, 75, 100]}
+                                            rowsPerPageOptions={[24, 75, 100]}
                                             colSpan={3}
                                             count={rows.length}
                                             rowsPerPage={rowsPerPage}
