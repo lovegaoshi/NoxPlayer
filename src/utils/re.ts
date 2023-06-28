7cf2d0abb9e3ef45414df016adffef34d8a4a2db
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2023-06-28 08:23:27 -0700

    Update re.ts

--- src/utils/re.ts
+++ src/utils/re.ts
@@ -269,6 +269,7 @@ export const reExtractSongName = (
       filename = extractWith(extractParenthesis(filename), [
         /【黑泽诺亚NOIR】(.+) - (.+)/,
         /【黑泽诺亚NOIR】(.+)/,
+        /【黑泽诺亚】【歌切】(.+) - (.+)/,
         /【黑泽诺亚】【歌切】(.+)/,
         /【黑泽诺亚】【.+】(.+)/,
         /【黑泽诺亚】(.+)/,
