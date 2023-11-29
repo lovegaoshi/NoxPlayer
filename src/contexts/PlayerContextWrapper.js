e2797f474e888479f64e10f694b0a43834fc0f88
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2023-11-28 20:00:27 -0800

    refactor: depreciate timerContext
    its dumber but oh well....

--- src/contexts/PlayerContextWrapper.js
+++ src/contexts/PlayerContextWrapper.js
@@ -1,11 +1,6 @@
 import React, { useState } from 'react';
 import { CurrentAudioProvider } from './CurrentAudioContext';
-import { TimerProvider } from './TimerContext';
 
 export default function playerContext(props) {
-  return (
-    <CurrentAudioProvider>
-      <TimerProvider>{props.children}</TimerProvider>
-    </CurrentAudioProvider>
-  );
+  return <CurrentAudioProvider>{props.children}</CurrentAudioProvider>;
 }
