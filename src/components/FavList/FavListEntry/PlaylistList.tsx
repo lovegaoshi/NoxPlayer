8c82657d2d7b424064b64963c73cae592071e743
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2023-12-05 05:48:44 -0800

    refactor: project structure

--- src/components/FavList/FavListEntry/PlaylistList.tsx
+++ src/components/FavList/FavListEntry/PlaylistList.tsx
@@ -1,69 +1,127 @@
 import React from 'react';
+import List from '@mui/material/List';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 import useFavList from '@hooks/useFavList';
+import { ScrollBar } from '@styles/styles';
 import usePlayer from '@hooks/usePlayer';
+import { useNoxSetting } from '@APM/stores/useApp';
+// eslint-disable-next-line import/no-unresolved
+import { AddFavDialog, NewFavDialog } from '@components/dialogs/AddFavDialog';
 import { skinPreset } from '@styles/skin';
-import { PlaylistEntry } from './PlaylistEntry';
+import { PlaylistEntry, SearchlistEntry } from './PlaylistEntry';
 
 const { colorTheme } = skinPreset;
 
 export default function PlaylistList() {
   const { onPlayAllFromFav } = usePlayer({});
+  const favoritePlaylist = useNoxSetting((state) => state.favoritePlaylist);
   const {
     playlists,
     playlistIds,
+    searchList,
     setSelectedList,
     setSongsStoredAsNewFav,
+    openNewDialog,
     setOpenNewDialog,
+    openAddDialog,
+    actionFavId,
+    actionFavSong,
+
+    onNewFav,
     handleDeleteFavClick,
     handleAddToFavClick,
+    onAddFav,
     onDragEnd,
   } = useFavList();
 
   return (
-    <DragDropContext onDragEnd={onDragEnd}>
-      <Droppable droppableId='droppable'>
-        {(provided, snapshot) => (
-          <div {...provided.droppableProps} ref={provided.innerRef}>
-            {playlistIds.map((item, index) => (
-              <Draggable
-                key={`item-${index}`}
-                draggableId={`item-${index}`}
-                index={index}
-              >
-                {(provided2, snapshot2) => (
-                  <div
-                    ref={provided2.innerRef}
-                    {...provided2.draggableProps}
-                    {...provided2.dragHandleProps}
-                    style={{
-                      background: snapshot2.isDragging
-                        ? colorTheme.clickHoldBackground
-                        : null,
-                      ...provided2.draggableProps.style,
-                    }}
-                  >
-                    <PlaylistEntry
-                      key2={String(index)}
-                      playlist={playlists[item]!}
-                      setSelectedList={setSelectedList}
-                      handleAddToFavClick={handleAddToFavClick}
-                      onPlayAllFromFav={onPlayAllFromFav}
-                      handleCreateAsFavClick={(v) => {
-                        setSongsStoredAsNewFav(v);
-                        setOpenNewDialog(true);
-                      }}
-                      handleDeleteFavClick={handleDeleteFavClick}
-                    />
-                  </div>
-                )}
-              </Draggable>
-            ))}
-            {provided.placeholder}
-          </div>
+    <React.Fragment>
+      <List
+        style={{ overflow: 'auto', maxHeight: 'calc(100% - 50px)' }}
+        className={ScrollBar().root}
+        sx={{ width: '100%' }}
+        component='nav'
+      >
+        <SearchlistEntry
+          key2='key'
+          playlist={searchList}
+          setSelectedList={setSelectedList}
+          handleAddToFavClick={handleAddToFavClick}
+          onPlayAllFromFav={onPlayAllFromFav}
+          handleCreateAsFavClick={(v) => {
+            setSongsStoredAsNewFav(v);
+            setOpenNewDialog(true);
+          }}
+          handleDeleteFavClick={handleDeleteFavClick}
+        />
+        {false && (
+          <SearchlistEntry
+            key2='key'
+            playlist={favoritePlaylist}
+            setSelectedList={setSelectedList}
+            handleAddToFavClick={handleAddToFavClick}
+            onPlayAllFromFav={onPlayAllFromFav}
+            handleCreateAsFavClick={(v) => {
+              setSongsStoredAsNewFav(v);
+              setOpenNewDialog(true);
+            }}
+            handleDeleteFavClick={handleDeleteFavClick}
+          />
         )}
-      </Droppable>
-    </DragDropContext>
+        <DragDropContext onDragEnd={onDragEnd}>
+          <Droppable droppableId='droppable'>
+            {(provided, snapshot) => (
+              <div {...provided.droppableProps} ref={provided.innerRef}>
+                {playlistIds.map((item, index) => (
+                  <Draggable
+                    key={`item-${index}`}
+                    draggableId={`item-${index}`}
+                    index={index}
+                  >
+                    {(provided2, snapshot2) => (
+                      <div
+                        ref={provided2.innerRef}
+                        {...provided2.draggableProps}
+                        {...provided2.dragHandleProps}
+                        style={{
+                          background: snapshot2.isDragging
+                            ? colorTheme.clickHoldBackground
+                            : null,
+                          ...provided2.draggableProps.style,
+                        }}
+                      >
+                        <PlaylistEntry
+                          key2={String(index)}
+                          playlist={playlists[item]!}
+                          setSelectedList={setSelectedList}
+                          handleAddToFavClick={handleAddToFavClick}
+                          onPlayAllFromFav={onPlayAllFromFav}
+                          handleCreateAsFavClick={(v) => {
+                            setSongsStoredAsNewFav(v);
+                            setOpenNewDialog(true);
+                          }}
+                          handleDeleteFavClick={handleDeleteFavClick}
+                        />
+                      </div>
+                    )}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
+      </List>
+      <NewFavDialog id='NewFav' openState={openNewDialog} onClose={onNewFav} />
+      <AddFavDialog
+        id='AddFav'
+        openState={openAddDialog}
+        onClose={onAddFav}
+        fromList={actionFavId}
+        songs={actionFavSong === undefined ? [] : [actionFavSong]}
+        // MenuProps={{ style: { maxHeight: 200 } }}
+      />
+    </React.Fragment>
   );
 }
--- src/components/FavList/Playlists.tsx
+++ src/components/FavList/Playlists.tsx
@@ -1,39 +1,17 @@
 import React from 'react';
-import List from '@mui/material/List';
 import Divider from '@mui/material/Divider';
 import Box from '@mui/material/Box';
 
-import { ScrollBar } from '@styles/styles';
 import useFavList from '@hooks/useFavList';
 import { skinPreset } from '@styles/skin';
-import usePlayer from '@hooks/usePlayer';
-// eslint-disable-next-line import/no-unresolved
-import { AddFavDialog, NewFavDialog } from '@components/dialogs/AddFavDialog';
 import Search from './FavListHeader/Search';
 import PlaylistHeaderButtons from './FavListHeader/PlaylistHeaderButtons';
 import PlaylistList from './FavListEntry/PlaylistList';
-import { SearchlistEntry } from './FavListEntry/PlaylistEntry';
 
 const { colorTheme } = skinPreset;
 
 export default function () {
-  const { onPlayAllFromFav } = usePlayer({});
-  const {
-    setSearchInputVal,
-    searchList,
-    setSelectedList,
-    setSongsStoredAsNewFav,
-    openNewDialog,
-    setOpenNewDialog,
-    openAddDialog,
-    actionFavId,
-    actionFavSong,
-
-    onNewFav,
-    handleDeleteFavClick,
-    handleAddToFavClick,
-    onAddFav,
-  } = useFavList();
+  const { setSearchInputVal } = useFavList();
 
   return (
     <React.Fragment>
@@ -54,36 +32,8 @@ export default function () {
         />
         <Divider light />
 
-        <List
-          style={{ overflow: 'auto', maxHeight: 'calc(100% - 50px)' }}
-          className={ScrollBar().root}
-          sx={{ width: '100%' }}
-          component='nav'
-        >
-          <SearchlistEntry
-            key2='key'
-            playlist={searchList}
-            setSelectedList={setSelectedList}
-            handleAddToFavClick={handleAddToFavClick}
-            onPlayAllFromFav={onPlayAllFromFav}
-            handleCreateAsFavClick={(v) => {
-              setSongsStoredAsNewFav(v);
-              setOpenNewDialog(true);
-            }}
-            handleDeleteFavClick={handleDeleteFavClick}
-          />
-          <PlaylistList />
-        </List>
+        <PlaylistList />
       </Box>
-      <NewFavDialog id='NewFav' openState={openNewDialog} onClose={onNewFav} />
-      <AddFavDialog
-        id='AddFav'
-        openState={openAddDialog}
-        onClose={onAddFav}
-        fromList={actionFavId}
-        songs={actionFavSong === undefined ? [] : [actionFavSong]}
-        // MenuProps={{ style: { maxHeight: 200 } }}
-      />
     </React.Fragment>
   );
 }
