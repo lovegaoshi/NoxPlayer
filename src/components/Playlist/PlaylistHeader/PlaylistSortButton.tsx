22107e6c540cacd67f372f8bb71f1f6816095ea5
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2023-12-20 08:40:04 -0800

    refactor: GenericSelectDialog

--- src/components/Playlist/PlaylistHeader/PlaylistSortButton.tsx
+++ src/components/Playlist/PlaylistHeader/PlaylistSortButton.tsx
@@ -2,18 +2,12 @@ import React from 'react';
 import Tooltip from '@mui/material/Tooltip';
 import IconButton from '@mui/material/IconButton';
 import SwapVertIcon from '@mui/icons-material/SwapVert';
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogTitle from '@mui/material/DialogTitle';
-import Radio from '@mui/material/Radio';
-import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
-import FormControl from '@mui/material/FormControl';
 import Checkbox from '@mui/material/Checkbox';
 import FormGroup from '@mui/material/FormGroup';
 
+// eslint-disable-next-line import/no-unresolved
+import GenericSelectDialog from '@components/dialogs/GenericSelectDialog';
 import { SORT_OPTIONS } from '@enums/Playlist';
 
 interface Props {
@@ -25,37 +19,31 @@ interface Props {
   playlist: NoxMedia.Playlist;
 }
 
-interface DialogProps extends Props {
-  openState: boolean;
-  onClose: () => void;
-}
-
-function PlaylistSortDialog({
-  openState,
-  onClose,
-  playlist,
-  sortPlaylist,
-}: DialogProps) {
-  const [sortOption, setSortOption] = React.useState(
-    SORT_OPTIONS.PREVIOUS_ORDER,
-  );
+export default function ({ sortPlaylist, playlist }: Props) {
+  const [showDialog, setShowDialog] = React.useState(false);
   const [ascending, setAscending] = React.useState(false);
-  const handleSort = () => {
-    onClose();
+  const sortOptions = Object.values(SORT_OPTIONS);
+
+  const handleSort = (sortOption: SORT_OPTIONS) => {
     sortPlaylist(sortOption, ascending, playlist);
   };
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSortOption((event.target as HTMLInputElement).value as SORT_OPTIONS);
-  };
-
-  React.useEffect(() => {
-    setSortOption(playlist.sort ?? SORT_OPTIONS.PREVIOUS_ORDER);
-  }, [playlist]);
 
   return (
-    <Dialog open={openState} onClose={onClose}>
-      <DialogTitle>{`SORT歌单 ${playlist.title} by:`}</DialogTitle>
-      <DialogContent>
+    <React.Fragment>
+      <Tooltip title='SORT'>
+        <IconButton size='large' onClick={() => setShowDialog(true)}>
+          <SwapVertIcon />
+        </IconButton>
+      </Tooltip>
+      <GenericSelectDialog
+        visible={showDialog}
+        options={sortOptions}
+        selectTitle='SORT歌单'
+        title={`SORT歌单 ${playlist.title} by:`}
+        defaultIndex={playlist.sort}
+        onClose={() => setShowDialog(false)}
+        onSubmit={handleSort}
+      >
         <FormGroup>
           <FormControlLabel
             control={
@@ -68,48 +56,7 @@ function PlaylistSortDialog({
             label='升序?'
           />
         </FormGroup>
-        <FormControl>
-          <RadioGroup
-            aria-labelledby='demo-controlled-radio-buttons-group'
-            name='controlled-radio-buttons-group'
-            value={sortOption}
-            onChange={handleChange}
-          >
-            {Object.entries(SORT_OPTIONS).map(([key, value]) => (
-              <FormControlLabel
-                value={value}
-                control={<Radio />}
-                label={value}
-                key={key}
-              />
-            ))}
-          </RadioGroup>
-        </FormControl>
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={onClose}>取消</Button>
-        <Button onClick={handleSort}>确认</Button>
-      </DialogActions>
-    </Dialog>
-  );
-}
-
-export default function ({ sortPlaylist, playlist }: Props) {
-  const [showDialog, setShowDialog] = React.useState(false);
-
-  return (
-    <React.Fragment>
-      <Tooltip title='SORT'>
-        <IconButton size='large' onClick={() => setShowDialog(true)}>
-          <SwapVertIcon />
-        </IconButton>
-      </Tooltip>
-      <PlaylistSortDialog
-        openState={showDialog}
-        onClose={() => setShowDialog(false)}
-        sortPlaylist={sortPlaylist}
-        playlist={playlist}
-      />
+      </GenericSelectDialog>
     </React.Fragment>
   );
 }
