ebef84f0b7c7fdbdba121fed0aa3fbb9a1efa16c
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2023-12-21 09:03:06 -0800

    Update nyiaibu.ts

--- src/styles/skins/nyiaibu.ts
+++ src/styles/skins/nyiaibu.ts
@@ -23,6 +23,7 @@ export default () =>
     playerBackgroundMobileVideo: randomPortraitBackground.includes('.mp4'),
     playerBackground: () =>
       fetchVideoPlayUrl(randomChoice(['BV1rh4y1b7wU', 'BV1SH4y1o7rk'])), // BV1Sb4y1i79D
+    // https://i0.hdslb.com/bfs/new_dyn/df6cf89d338ab29e7b776eec9a81fd5c27990035.jpg
     playerBackgroundVideo: true,
     // playerBackground: async () => new Promise<string>((resolve) => { resolve('https://i0.hdslb.com/bfs/new_dyn/aae8c009d55b9db3472c1059b32cf16c1817527011.jpg'); }),
     gifs,
