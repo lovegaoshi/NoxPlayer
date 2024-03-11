7b671f537186cf56421ffc5d670eac0687a6f7ef
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2024-03-11 12:28:31 -0700

    fix: local search

--- src/utils/mediafetch/local.ts
+++ src/utils/mediafetch/local.ts
@@ -1,1 +1,8 @@
-export default {};
+export default {
+  regexSearchMatch: /NOT_IMPLEMETED_ERROR/,
+  regexFetch: () => undefined,
+  regexResolveURLMatch: /NOT_IMPLEMENTED_ERROR/,
+  resolveURL: () => undefined,
+  refreshSong: () => undefined,
+  resolveArtwork: () => undefined,
+};
