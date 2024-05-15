9731a62aa58c7ed195fc5e929b330382ba845119
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2024-05-15 09:39:55 -0700

    chore: dep upgrade

--- src/components/lyric/LyricSearchBar.tsx
+++ src/components/lyric/LyricSearchBar.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 
