1c5499ede84efdb4d8214e7a4a256d7219192294
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2025-09-30 14:56:55 -0700

    chore: yarn dep

--- src/utils/mediafetch/ytbi.ts
+++ src/utils/mediafetch/ytbi.ts
@@ -1,10 +1,12 @@
 import { ClientType, Innertube } from 'youtubei.js/web';
+import { GHCacher } from '@APM/utils/fakeMMKV';
 
 const ytClient = Innertube.create({
+  cache: new GHCacher(),
   retrieve_player: true,
   enable_session_cache: false,
   generate_session_locally: false,
-  client_type: ClientType.IOS,
+  client_type: ClientType.WEB_EMBEDDED,
   fetch: async (input, init?: RequestInit) => {
     // Modify the request
     // and send it to the proxy
