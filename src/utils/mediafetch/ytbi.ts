b32a3cb8183e2bcb2b920b27bfc614220982b37c
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2025-10-12 21:45:13 -0700

    feat: ytbi 16.0.0

--- src/utils/mediafetch/ytbi.ts
+++ src/utils/mediafetch/ytbi.ts
@@ -1,5 +1,31 @@
-import { ClientType, Innertube } from 'youtubei.js/web';
+import { ClientType, Innertube, Platform } from 'youtubei.js/web';
+import { BuildScriptResult, VMPrimative } from 'youtubei.js/dist/src/types';
+
 import { GHCacher } from '@APM/utils/fakeMMKV';
+import evalCode from '../eval';
+
+Platform.shim.eval = async (
+  data: BuildScriptResult,
+  env: Record<string, VMPrimative>,
+) => {
+  const properties = [];
+
+  if (env.n) {
+    properties.push(`n: exportedVars.nFunction("${env.n}")`);
+  }
+
+  if (env.sig) {
+    properties.push(`sig: exportedVars.sigFunction("${env.sig}")`);
+  }
+  // HACK: sandboxed iframe cant access eg sessionStorage and throws and error
+  // mapping globalThis. luckily this isnt really used
+  // if broken ,check the executing code...
+  const code = `${data.output.replace('const window = Object.assign({}, globalThis)', 'const window = {}')}\nreturn { ${properties.join(', ')} }`;
+  const result = await evalCode(code);
+  return JSON.parse(result);
+};
+
+window.evalCode = evalCode;
 
 const ytClient = Innertube.create({
   cache: new GHCacher(),
@@ -12,7 +38,8 @@ const ytClient = Innertube.create({
     // and send it to the proxy
 
     // fetch the URL
-
+    // @ts-expect-error their headers is actually a map
+    init?.headers?.set('origin', 'https://www.youtube.com');
     return fetch(input, init);
   },
 });
